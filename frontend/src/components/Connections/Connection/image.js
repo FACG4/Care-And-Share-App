0081fd9fe4328b8f488904418d80a2119b6e1687
eman <eman_h_2009@live.com>
2018-07-31 13:27:37 +0300

    solve problem in component

--- frontend/src/components/Connections/Connection/image.js
+++ frontend/src/components/Connections/Connection/image.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 
-const Image = ({ openModel, name }) => (
+const Image = ({ openModel, name, id }) => (
   <div className="FrindesImage" onClick={openModel}>
-    <img src="https://www.shareicon.net/data/2016/08/05/806962_user_512x512.png" alt="avata" id={props.id} />
+    <img src="https://www.shareicon.net/data/2016/08/05/806962_user_512x512.png" alt="avata" id={id} />
     <h3>
       {name}
     </h3>
