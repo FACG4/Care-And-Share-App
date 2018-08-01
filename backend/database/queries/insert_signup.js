85772869bbec53af4989d67342e353ae817b03ba
eman <eman_h_2009@live.com>
2018-08-01 11:48:37 +0300

    signUp back

--- backend/database/queries/insert_signup.js
+++ backend/database/queries/insert_signup.js
@@ -2,16 +2,19 @@ const connection = require('./../db_connection');
 
 const signupInsert = (fullName, userName, email, password, cb) => {
   const sql = {
-    text: 'INSRT INTO users(full_name, user_name, email, password) VALUES($1,$2,$3,$4) ',
+    text: 'INSERT INTO users(full_name, user_name, email, password) VALUES($1,$2,$3,$4) ',
     values: [fullName, userName, email, password],
   };
   connection.query(sql, (err, res) => {
+
+    
     if (err) cb(err);
     else {   
       cb(null, res.rows);
     }
   });
 };
+//upload
 
 module.exports = signupInsert;
 
