a54bec74bc1f1c395a854dd4b1f93891da8b2060
Ahmed Shatat <a.shatat@hotmail.com>
2018-08-04 22:27:45 +0300

    public profile backend api

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+
 const carers = require('./carers');
 const login = require('./login');
+const { postProfile, getProfile } = require('./profile');
 
 router.post('/login', login.post)
 router.post('/carers', carers.post)
-
+router.get('/api/profile', getProfile)
+router.post('/api/profile', postProfile)
 
 module.exports = router;
