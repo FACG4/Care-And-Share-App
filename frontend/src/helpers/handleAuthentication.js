f290a327f280395ff6cfd6564c5c557704147a1b
Ahmed Shatat <a.shatat@hotmail.com>
2018-08-06 17:20:10 +0300

    remove jwt verify

--- frontend/src/helpers/handleAuthentication.js
+++ frontend/src/helpers/handleAuthentication.js
@@ -1,23 +1,23 @@
-const secret = process.env.REACT_APP_SECRET;
-const jwt = require('jsonwebtoken');
+// const secret = process.env.REACT_APP_SECRET;
+// const jwt = require('jsonwebtoken');
 
 const sessionCheckError = (token) => {
-  if (token) {
-    return jwt.verify(token, secret, (err, decoded) => {
-      if (err) {
-        return {
-          status: false,
-        };
-      }
-      return {
-        status: true,
-        id: decoded.id,
-      };
-    });
-  }
+  // if (token) {
+  //   return jwt.verify(token, secret, (err, decoded) => {
+  //     if (err) {
+  //       return {
+  //         status: false,
+  //       };
+  //     }
+  //     return {
+  //       status: true,
+  //       id: decoded.id,
+  //     };
+  //   });
+  // }
 
   return {
-    status: false,
+    status: !!token,
   };
 };
 
