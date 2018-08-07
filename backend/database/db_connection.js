b1888d8509709381e2f74e5dba27b07769d9d6a7
Ahmed Shatat <a.shatat@hotmail.com>
2018-08-08 00:00:23 +0300

    fix ssl problem between development and production

--- backend/database/db_connection.js
+++ backend/database/db_connection.js
@@ -1,8 +1,18 @@
 const { Pool } = require('pg');
 require('env2')('./config.env');
 
-if (!process.env.DATABASE_URL) {
+let dbUrl, ssl;
+if(process.env.PORT) {
+  // deployment
+  dbUrl = process.env.DATABASE_URL;
+  ssl = true;
+} else {
+  // development
+  dbUrl = process.env.DB_URL;
+  ssl = false;
+}
+if (!dbUrl) {
   throw new Error('DB url not found ');
 } else {
-  module.exports = new Pool({ connectionString: process.env.DATABASE_URL, ssl: false });
+  module.exports = new Pool({ connectionString: dbUrl, ssl });
 }
