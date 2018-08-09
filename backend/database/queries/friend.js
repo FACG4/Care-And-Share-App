222aba29f9b4b59ddd398d709b498b2f391ee079
Ahmed Shatat <a.shatat@hotmail.com>
2018-08-09 14:09:50 +0300

    fix massage and friend avatar

--- backend/database/queries/friend.js
+++ backend/database/queries/friend.js
@@ -2,7 +2,7 @@ const connection = require('./../db_connection');
 
 const selectFriend = (id, cb) => {
   const sql = {
-    text: 'select u.id as user_id, c.id, u.full_name from users as u, connections as c where (u.id =c.receiver_user_id and c.sender_user_id = $1 and c.relation_state = $2) or (u.id = c.sender_user_id and c.receiver_user_id = $1 and c.relation_state = $2)',
+    text: 'select u.id as user_id, c.id, u.full_name, u.image from users as u, connections as c where (u.id =c.receiver_user_id and c.sender_user_id = $1 and c.relation_state = $2) or (u.id = c.sender_user_id and c.receiver_user_id = $1 and c.relation_state = $2)',
     values: [id, "approved"]
   }
 
